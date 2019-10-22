741232fcdc37d469ee5b0bb9786ed6192d73c1ad
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2019-10-22 05:05:52 +0300

    Add Time extension for DateTime

--- lib/extensions/date-time.dart
+++ lib/extensions/date-time.dart
@@ -1,3 +1,9 @@
 extension DateTimeEx on DateTime {
   DateTime get Date => DateTime(this.year, this.month, this.day);
+  Duration get Time => Duration(
+    hours: this.hour, 
+    minutes: this.minute, 
+    seconds: this.second, 
+    milliseconds: this.millisecond, 
+    microseconds: this.microsecond);
 }
